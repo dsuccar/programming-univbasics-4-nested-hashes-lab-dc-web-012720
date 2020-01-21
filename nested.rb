b0dde6b2939c126c4f270688c7e66030e0caf7d6
Daniel Succar <danielsuccar@icloud.com>
2020-01-21 23:09:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-hopper = programmer_hash[:grace_hopper][:known_for]["COBOL"]
+programmer_hash[:grace_hopper][:known_for] = "COBOL"
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
