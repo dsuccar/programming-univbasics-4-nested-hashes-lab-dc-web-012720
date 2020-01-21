631a9712a608530261b9ea8e67def4a2037a2026
Daniel Succar <danielsuccar@icloud.com>
2020-01-21 23:19:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,6 +67,11 @@ def adding_matz
 # }
 
 	programmer_hash =
+	{
+	 :yukihiro_matsumoto => {
+	   :known_for => "Ruby",
+	   :languages => ["LISP", "C"]
+	}
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -81,6 +86,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+	 }
 
 
 end
